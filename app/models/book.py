83c4eca066978603e78e449a07e988c9e22b74c2
GehadKassap <gohadkasaa@gmail.com>
2024-11-05 15:11:03 +0200

    __REFACTOR__ models to be inside folder

---
+++ app/models/book.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+from ..extensions import db
+
+
+class Book(db.Model):
+    __tablename__ = 'books'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150), nullable=False)
+    author = db.Column(db.String(150), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(
+        db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
